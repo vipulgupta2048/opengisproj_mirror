abd0b353426f1e2aa2e67efb047568050aa6bc8e
Parth Sharma <parth261297@gmail.com>
2017-07-29 19:29:30 +0530

    Fixed Paswwsword field

--- opengisproj/accounts/forms.py
+++ opengisproj/accounts/forms.py
@@ -5,4 +5,4 @@ from django import forms
 #The following implements Bootstrap CSS to the login fields
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label="Username", max_length=30, widget=forms.TextInput(attrs={'class':'form-control', 'name':'username'}))
-    password = forms.CharField(label="Password", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password'}))
+    password = forms.CharField(label="Password", max_length=30, widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
