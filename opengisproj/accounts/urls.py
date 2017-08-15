bcbdcb5f6127a3caee05f9211464620413289371
thisisayush <thisisayushaa@gmail.com>
2017-08-15 10:59:51 +0530

    Fix Forms not found bug

--- opengisproj/accounts/urls.py
+++ opengisproj/accounts/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout
 from django.views.generic import RedirectView
-from forms import LoginForm
+from .forms import LoginForm
 from . import views
 
 
